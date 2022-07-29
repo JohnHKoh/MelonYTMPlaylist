e8d808d109b71680d4efab1bde62c1f98d0f621c
JohnHKoh <jhk5ec@virginia.edu>
2022-07-29 10:17:29 -0400

    Add support for manual fixes

--- src/daily.py
+++ src/daily.py
@@ -16,18 +16,38 @@ playlist = ytmusic.get_playlist(playlistId)
 tracks = playlist['tracks']
 if (len(tracks) > 0):
     print("Clearing playlist...")
-    ytmusic.remove_playlist_items(playlistId, tracks)
-    print("Playlist cleared")
+    clear_response = ytmusic.remove_playlist_items(playlistId, tracks)
+    if "SUCCEEDED" in clear_response:
+        print("Playlist cleared")
+    else:
+        print("Could not clear playlist")
+        print(json.dumps(clear_response))
+    
 songs = melon_retriever.get_daily()
 toAdd = []
+
+f2 = open('../data/manual_fixes.json', 'r', encoding='utf-8')
+manual_fixes = json.load(f2)
+
 for i in range(100):
-    print("#{}: Searching for {} {} {}".format(i + 1, songs[i].title, songs[i].artist, songs[i].album))
-    results = ytmusic.search("{} {} {}".format(songs[i].title, songs[i].artist, songs[i].album), "songs")
-    toAdd.append(results[0]['videoId']) # assume the top result is correct
+    search_query = "{} {} {}".format(songs[i].title, songs[i].artist, songs[i].album).strip()
+    print("#{}: Searching for {}".format(i + 1, search_query))
+    if search_query in manual_fixes:
+        print("Manual fix for {}".format(search_query))
+        result = manual_fixes[search_query]
+    else:
+        results = ytmusic.search("{} {} {}".format(songs[i].title, songs[i].artist, songs[i].album), "songs")
+        result = results[0]['videoId'] # assume the top result is correct
+    
+    toAdd.append(result) 
     
 print("Adding playlist items...")
-ytmusic.add_playlist_items(playlistId, toAdd, duplicates=True)
-print("All items added")
+add_response = ytmusic.add_playlist_items(playlistId, toAdd, duplicates=True)
+if 'status' in add_response and 'SUCCEEDED' in add_response['status']:
+    print("All items added")
+else:
+    print("Items could not added")
+    print(json.dumps(add_response))
 
 print("Updating playlist description...")
 today = date.today();
@@ -44,5 +64,9 @@ Updated for: {}
 This playlist is auto-generated.
 View the code here: https://github.com/JohnHKoh/MelonYTMPlaylist
 """.format(today_str)
-ytmusic.edit_playlist(playlistId, description=description)
-print("Playlist description updated")
+edit_response = ytmusic.edit_playlist(playlistId, description=description)
+if 'SUCCEEDED' in edit_response:
+    print("Playlist description updated")
+else:
+    print("Playlist description could not be updated")
+    print(json.dumps(edit_response))
